70574f53c0c475e622a0fe57189711f9e6a1ea65
ayushjaiswal-aj <ayushjaiswalaj@outlook.com>
2019-01-05 14:42:16 +0530

    adding c program for prime number dermination

---
+++ prime no_ determination.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+   int x,y;
+   printf ("enter any number");
+   scanf("%d",&x);
+   
+        
+   } 
+   
+   
+   
+   if (y==x)
+   printf ("\nnumber is prime");
+   
+   
+   
+}
+    
