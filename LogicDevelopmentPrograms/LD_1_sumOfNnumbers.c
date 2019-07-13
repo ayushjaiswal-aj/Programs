c33e630960b4b7c0910d68de5d531b771ab651f0
ayushjaiswal-aj <ayushjaiswalaj@outlook.com>
2019-07-13 17:49:13 +0530

    more changes

--- LogicDevelopmentPrograms/LD_1_sumOfNnumbers.c
+++ LogicDevelopmentPrograms/LD_1_sumOfNnumbers.c
@@ -14,11 +14,21 @@ int add(int n){
 		}
 	return sum;
 	}
+/*
+ *This is the optimized funtion of above add funtion 
+ *With use of formula the time consumption of program is reduced
+ *It became more efitiant in long run
+ */
+int add_V1(int m){
+	int a = (m*(m+1)/2);
+	return a;
+	}
 int main(){
 
 	printf("%d is the sum of numbers till %d\n",add(5),5);
 	printf("%d is the sum of numbers till %d\n",add(1),1);
-	printf("%d is the sum of numbers till %d\n",add(0),0);
+	printf("%d is the sum of numbers till %d\n",add(10),10);
+	printf("%d is the sum of numbers till %d\n",add_V1(10),10);
 	return 0;
 
 	}
